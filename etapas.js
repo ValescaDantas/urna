844d69ad1987fc054ead3d48444b260170b1efdd
ValescaDantas <86363818+ValescaDantas@users.noreply.github.com>
2021-10-08 16:24:19 -0300

    Add files

---
+++ etapas.js
@@ -0,0 +1,51 @@
+let etapas = [
+{
+titulo:'VEREADOR',
+numeros: 5,
+candidatos: [
+    {
+        numero:38111,
+        nome:'Fulano de tal',
+        partido: 'ABC',
+        fotos: [
+            {url:'pref.jpg', legenda: 'Vereador',small: false}
+        ]
+    },
+{ 
+    numero:'77222',
+    nome:'Beltrano da Silva',
+    partido: 'DEF',
+    fotos: [
+        {url:'w.png', legenda: 'Vereador', small: false}
+    ]
+},
+]
+},
+{
+    titulo:'PREFEITO',
+    numeros: 2,
+    candidatos:[
+{
+    numero:'99',
+    nome:'Ciclano',
+    partido: 'ABC',
+    vice:'Cic',
+    fotos: [
+        {url:'pref.jpg', legenda: 'Prefeito'},
+        {url:'pref.jpg', legenda: 'Vice-Prefeito', small: true}
+    ]
+},
+{   
+    numero:'88',
+    nome:'Slavo',
+    partido: 'GHIJ',
+    vice:'Zul',
+    fotos: [
+        {url:'../img/pref.jpg', legenda: 'Prefeito'},
+        {url:'../img/pref.jpg', legenda: 'Vice-Prefeito', small: true}
+        ]
+      },
+
+     ]
+    }
+];
